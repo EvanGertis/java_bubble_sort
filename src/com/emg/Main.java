e0ec7469319a21d37ac1c1713e36d99f2a2be93f
EvanGertis <39931491+EvanGertis@users.noreply.github.com>
2019-07-09 07:21:09 -0400

    implemented

--- src/com/emg/Main.java
+++ src/com/emg/Main.java
@@ -3,7 +3,25 @@ package com.emg;
 public class Main {
 
     public static void main(String[] args) {
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
 
+        printArray(intArray);
+
+         for(int i = intArray.length - 1; i > 0; i--){
+             for(int j = 0; j < i; j++ ){
+                 if(intArray[j+1] < intArray[j])
+                     swap(intArray, j, j+1);
+             }
+         }
+
+         printArray(intArray);
+    }
+
+    public static void printArray(int[] array){
+        for(int i = 0; i < array.length; i++){
+            System.out.print(array[i] + " | ");
+        }
+        System.out.println();
     }
 
     public static void swap(int[] array, int i, int j){
